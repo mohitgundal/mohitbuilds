4b17646a0cb52ad7c6e5045033a8b9a5002ac073
MohitGundal09 <mohitgundal09@gmail.com>
2025-06-04 13:40:23 +0530

    edited vite.config.ts file

--- project-06-folder/vite.config.ts
+++ project-06-folder/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/project-06-folder/", // 👈 Add this line
+  base: "/project-06-react/",
   plugins: [react()],
 });
