e2bfbfc63d716ed2c829df713267a6cecfa3b6e2
MohitGundal09 <mohitgundal09@gmail.com>
2025-06-04 16:06:19 +0530

    renamed file

--- project-06-folder/vite.config.ts
+++ project-06-folder/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Note-Taking-App/",
   plugins: [react()],
-});
+  base: '/Note-Taking-App/'  // <-- IMPORTANT: match the folder name in mohitbuilds repo
+})
